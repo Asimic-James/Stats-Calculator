9bb98f00865ed65c211818c7997b0083e1e79321
asimic-james <jayasimic@gmail.com>
2024-02-01 20:36:28 +0100

    features

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ const getMode = (array) => {
   array.forEach((el) => {
     counts[el] = (counts[el] || 0) + 1;
   });
-  if (new Set(Object.values(counts))) {
-    
+  if (new Set(Object.values(counts)).size === 1) {
+    return null;
   }
-  return null;
+  
 }
 
 
